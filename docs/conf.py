0734379f7792786acae0bea0d222f334a25abf97
Joshua Whitehurst <jcw06@yahoo.com>
2018-02-15 00:40:57 -0500

    gray

--- docs/conf.py
+++ docs/conf.py
@@ -82,7 +82,11 @@ html_theme = 'alabaster'
 # documentation.
 #
 # html_theme_options = {}
-
+html_theme_options = {
+    'logo': '',
+    'gray_3': 'true',
+    
+}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
